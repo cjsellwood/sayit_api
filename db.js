f8e5c7caed4f9f9b46389a112bffe53b3ee56073
cjsellwood <c.sellwood22@gmail.com>
2021-05-17 21:12:30 +1000

    Add heroku db connection

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const { Pool } = require("pg");
+const connectionString = process.env.DB_URL;
 
 const pool = new Pool({
-  user: "postgres",
-  database: "sayit",
+  // Local
+  // user: "postgres",
+  // database: "sayit",
+  connectionString,
 });
 
 module.exports = {
